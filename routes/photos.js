ad742a7047393c9d2053bf0312d27c1605c8554a
vinitgaikwad0810 <vinit.gaikwad10@gmail.com>
2016-12-10 23:44:19 -0800

    Search Functionality

--- routes/photos.js
+++ routes/photos.js
@@ -2,103 +2,140 @@ var mongo = require("./mongo");
 var mongoURL = "mongodb://cmpe_280:cmpe_280@ds113628.mlab.com:13628/photogallery";
 
 
-var json_responses={};
+var json_responses = {};
 
 
-exports.getPhotos = function(req, res){
+exports.getPhotos = function (req, res) {
 
-	var id=req.params.id;
-	console.log("id:"+id);
+    var id = req.params.id;
+    console.log("id:" + id);
 
-	mongo.connect(mongoURL,function(){
-		var coll=mongo.collection('photo');
-		coll.find({
-		    "owner": id
-		}).toArray(function(err, photos){
-			if (photos) {
-				json_responses.status_code=200;
-				json_responses.data=photos;
-				console.log(photos);
-				res.send( json_responses);
+    mongo.connect(mongoURL, function () {
+        var coll = mongo.collection('photo');
+        coll.find({
+            "owner": id
+        }).toArray(function (err, photos) {
+            if (photos) {
+                json_responses.status_code = 200;
+                json_responses.data = photos;
+                console.log(photos);
+                res.send(json_responses);
 
-			} else {
-				json_responses.status_code=500;
-				console.log(err);
-				res.send(json_responses);
-			}
-		});
-	});
-
-};
-exports.getImageDetails= function(req, res){
-
-	var id=req.params.id;
-	console.log("id:"+id);
-
-	mongo.connect(mongoURL,function(){
-		var coll=mongo.collection('photo');
-		coll.findOne({
-		    "photo_id": id
-		},function(err, photos){
-			if (photos) {
-				json_responses.status_code=200;
-				json_responses.data=photos;
-				console.log(photos);
-				res.send( json_responses);
-
-			}else{
-				json_responses.status_code=500;
-				console.log(err);
-				res.send(json_responses);
-				}
-			});
-	});
-};
-exports.getMyBuys = function(req, res){
-
-	var id=req.params.id;
-	console.log("id:"+id);
-
-	mongo.connect(mongoURL,function(){
-		var coll=mongo.collection('photo');
-		coll.find({
-		    "bought_by": id
-		}).toArray(function(err, photos){
-			if (photos) {
-				json_responses.status_code=200;
-				json_responses.data=photos;
-				console.log(photos);
-				res.send( json_responses);
-
-			} else {
-				json_responses.status_code=500;
-				console.log(err);
-				res.send(json_responses);
-			}
-		});
-	});
+            } else {
+                json_responses.status_code = 500;
+                console.log(err);
+                res.send(json_responses);
+            }
+        });
+    });
 
 };
 
 
-exports.uploadPhotos = function(req, res){
+exports.getPhotosByTag = function (req, res) {
 
-	var docs=req.body;
-	console.log("docs:"+docs);
-	mongo.connect(mongoURL,function() {
+    var tag = req.params.tag;
+    console.log("Tag");
+    console.log(tag);
+    tagArr = [];
+    tagArr[tagArr.length] = tag;
 
-		mongo.collection('photo').insertOne(req.body,function(err, result) {
+    mongo.connect(mongoURL, function () {
+        var coll = mongo.collection('photo');
+        coll.find({
+            "imageData.tags": tag
+        }).toArray(function (err, photos) {
 
-				if(err){
-					json_responses.status_code=500;
-					console.log(err);
-					res.send(json_responses);
-				}else{
-					json_responses.status_code=200;
-					res.send( json_responses);
-				}
-	  	});
+                if (photos) {
+                    json_responses.status_code = 200;
+                    json_responses.data = photos;
+                    console.log(photos);
+                    res.send(json_responses);
 
-		});
+                } else {
+                    json_responses.status_code = 500;
+                    console.log(err);
+                    res.send(json_responses);
+                }
+            }
+        );
+    });
+
+
+}
+
+
+exports.getImageDetails = function (req, res) {
+
+    var id = req.params.id;
+    console.log("id:" + id);
+
+    mongo.connect(mongoURL, function () {
+        var coll = mongo.collection('photo');
+        coll.findOne({
+            "photo_id": id
+        }, function (err, photos) {
+            if (photos) {
+                json_responses.status_code = 200;
+                json_responses.data = photos;
+                console.log(photos);
+                res.send(json_responses);
+
+            } else {
+                json_responses.status_code = 500;
+                console.log(err);
+                res.send(json_responses);
+            }
+        });
+    });
+};
+exports.getMyBuys = function (req, res) {
+
+    var id = req.params.id;
+    console.log("id:" + id);
+
+    mongo.connect(mongoURL, function () {
+        var coll = mongo.collection('photo');
+        coll.find({
+            "bought_by": id
+        }).toArray(function (err, photos) {
+            if (photos) {
+                json_responses.status_code = 200;
+                json_responses.data = photos;
+                console.log(photos);
+                res.send(json_responses);
+
+            } else {
+                json_responses.status_code = 500;
+                console.log(err);
+                res.send(json_responses);
+            }
+        });
+    });
 
 };
+
+
+exports.uploadPhotos = function (req, res) {
+
+    var docs = req.body;
+    console.log("docs:" + docs);
+    mongo.connect(mongoURL, function () {
+
+        mongo.collection('photo').insertOne(req.body, function (err, result) {
+
+            if (err) {
+                json_responses.status_code = 500;
+                console.log(err);
+                res.send(json_responses);
+            } else {
+                json_responses.status_code = 200;
+                res.send(json_responses);
+            }
+        });
+
+    });
+
+};
+
+
